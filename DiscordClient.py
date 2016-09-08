179df79f572e4840eea4b97b8a70e601291dff9e
Eazy <antoine-28@hotmail.fr>
2016-09-08 21:43:39 +0200

    Reformatted code. English in commentaries

--- DiscordClient.py
+++ DiscordClient.py
@@ -1,37 +1,40 @@
 import discord
 import ask_api
 
-email = "myemail" # Replace myemail by the email for your bot account
-password = "mypassword" # Replace mypassword by the passowrd for your bot account
+email = "myemail"  # Replace myemail by the email for your bot account
+password = "mypassword"  # Replace mypassword by the passowrd for your bot account
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	print('------')
-	
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
 @client.event
 async def on_message(message):
-	# we do not want the bot to reply to itself
-	if message.author == client.user:
-		return
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
 
-	print("Nouveau message")
-	content = message.content
-	fin = len(content)
-	debut = content.find("[") +1
-	while debut > 0 :
-		#ss1 = content[ouvert: fin]
-		fin = content.find("]", debut)
-		requete = content[debut: fin]
-		print("Requete : "+requete)
-		if len(requete) > 2 :
-			msg_list = ask_api.ask_card(requete)
-			for msg in msg_list :
-				await client.send_message(message.channel, msg)
-		debut = content.find("[", fin) +1
-		
+    print("New message")
+    content = message.content
+    fin = len(content)
+    debut = content.find("[") + 1
+    while debut > 0:
+        # ss1 = content[ouvert: fin]
+        fin = content.find("]", debut)
+        requete = content[debut: fin]
+        print("Request : " + requete)
+        if len(requete) > 2:
+            msg_list = ask_api.ask_card(requete)
+            for msg in msg_list:
+                await client.send_message(message.channel, msg)
+        debut = content.find("[", fin) + 1
+
+
 client.run(email, password)
--- ask_api.py
+++ ask_api.py
@@ -1,45 +1,43 @@
 from mtgsdk import Card
-import json
 
-def ask_card (cardname) :
-	cards = Card.where(name = cardname).all()
-	l = []
-	n = []
-	i=0
-	for card in cards :
-		if i==20:
-			break
-		if card.name not in n:
-			types = card.types
-			msg = "**"+card.name+"**"+" "
-			if card.mana_cost is not None :
-				msg += card.mana_cost
-				#if card.set_name=='Unhinged' or card.set_name=='Unglued' :
-				msg += " - *"
-				last_set = card.printings[-1]
-				for set in card.printings :
-					msg += set
-					if set == last_set:
-						msg+="*"
-					else :
-						msg +=", "
-			msg += "\n"
-			if card.type is not None :
-				msg += card.type
-			if "Creature" in card.types :
-				msg += " "+card.power + "/" + card.toughness
-			elif "Planeswalker" in card.types :
-				msg+= ". Starting Loyalty "+str(card.loyalty)
-			if card.text is not None :
-				msg +="\n" + card.text
-			msg +="\n"
-			if cardname.lower() == card.name.lower():
-				l = [msg]
-				break
-			else:
-				l.append(msg)
-				i+=1
-				n.append(card.name)
-	return l
-	
-	# Renvoie un json
+
+def ask_card(cardname):
+    cards = Card.where(name=cardname).all()
+    l = []
+    n = []
+    i = 0
+    for card in cards:
+        if i == 20:
+            break
+        if card.name not in n:
+            msg = "**" + card.name + "**" + " "
+            if card.mana_cost is not None:
+                msg += card.mana_cost
+                # if card.set_name=='Unhinged' or card.set_name=='Unglued' :
+                msg += " - *"
+                last_set = card.printings[-1]
+                for set in card.printings:
+                    msg += set
+                    if set == last_set:
+                        msg += "*"
+                    else:
+                        msg += ", "
+            msg += "\n"
+            if card.type is not None:
+                msg += card.type
+            if "Creature" in card.types:
+                msg += " " + card.power + "/" + card.toughness
+            elif "Planeswalker" in card.types:
+                msg += ". Starting Loyalty " + str(card.loyalty)
+            if card.text is not None:
+                msg += "\n" + card.text
+            msg += "\n"
+            if cardname.lower() == card.name.lower():
+                l = [msg]
+                break
+            else:
+                l.append(msg)
+                i += 1
+                n.append(card.name)
+    return l
+
